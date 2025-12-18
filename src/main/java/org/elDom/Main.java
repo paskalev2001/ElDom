84e50d87f266996ec2b5d534b2d697ff71d2bd65
paskalev2001 <2001paskalpaskalev@gmail.com>
2025-12-18 16:10:43 +0200

    Extended smoke test for Apartments, Buildings and Resident.

--- src/main/java/org/elDom/Main.java
+++ src/main/java/org/elDom/Main.java
@@ -1,11 +1,60 @@
 package org.elDom;
 
 import org.elDom.configuration.SessionFactoryUtil;
+import org.elDom.entity.*;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.elDom.configuration.SessionFactoryUtil.*;
 
 public class Main {
     public static void main(String[] args) {
-        Session session = SessionFactoryUtil.getSessionFactory().openSession();
-        session.close();
+        //Session session = SessionFactoryUtil.getSessionFactory().openSession();
+        // Smoke tests
+        SessionFactory sessionFactory = SessionFactoryUtil.getSessionFactory();
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+
+            // Creating company associated with a Tax
+            Company company = new Company();
+            company.setName("TestCo");
+
+            Tax tax = new Tax();
+            tax.setCompany(company);
+
+            company.getTaxes().add(tax);
+
+            session.persist(company); // cascade = ALL => ще запише и Tax
+
+            // Adding a building with appartment and resident
+            Building b = new Building();
+            b.setAddress("Sofia, Test 1");
+
+            Apartment ap = new Apartment();
+            ap.setNumber("12");
+            ap.setFloor(3L);
+            ap.setArea(new BigDecimal("78.50"));
+
+            b.addApartment(ap);
+
+            Resident owner = new Resident();
+            owner.setFirstName("Ivan");
+            owner.setLastName("Ivanov");
+            owner.setBirthDate(LocalDate.of(1990, 5, 10));
+            owner.setUsesElevator(true);
+            owner.setRole(ResidentRole.OWNER);
+
+            ap.addResident(owner);
+
+            session.persist(b);
+            tx.commit();
+        }
+
+        //session.close();
     }
 }
